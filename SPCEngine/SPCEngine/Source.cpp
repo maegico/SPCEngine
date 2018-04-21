8b2c3485bf5e19e0acab07b94b5e7d1f010ef09e
Sean Connolly <seanconnolly@mail.rit.edu>
2018-04-20 23:17:46 -0400

    Add solution/project.

---
+++ SPCEngine/SPCEngine/Source.cpp
@@ -0,0 +1,10 @@
+//aiming for ECS:
+	//"Tutorial": https://www.raywenderlich.com/24878/introduction-to-component-based-architecture-in-games
+	//https://gamedev.stackexchange.com/questions/31473/what-is-the-role-of-systems-in-a-component-based-entity-architecture
+	//Wiki: http://entity-systems.wikidot.com/
+	//Unity examples: https://github.com/Unity-Technologies/EntityComponentSystemSamples
+
+int main()
+{
+	return 0;
+}
